c4d26d9d5c9f76625cd3acf174613b9cba71f54f
apadrones <apadrones@dfc56724-235c-11df-8901-e16673284e38>
2010-02-27 05:21:34 +0000

    Initial Version globalban-spanish v3.4.1 r104 (27/02/2010)(English / Spanish / French / Russian) from http://code.google.com/p/globalban-spanish/source/list

---
+++ trunk/GlobalBan/banned/languages/French/lan_footer.php
@@ -0,0 +1,16 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     esGlobalBan - Language File.
+|
+|     $Source: /cvsroot/banned/languages/French/lan_footer.php,v $
+|     $Revision: 1.0 $
+|     $Date: 2009/07/02 30:36:39 $
+|     $Author: Odonel $
++----------------------------------------------------------------------------+
+*/
+
+$LANFOOTER_001 = "[Panneau d'Administration]";
+$LANFOOTER_002 = "- Traduis par <a href='mailto:alpha.cssupport@gmail.com'>Owned</a> - &copy;2007-2010<br />";
+
+?>
---
+++ trunk/GlobalBan/banned/languages/Russian/lan_manageServerGroups.php
@@ -0,0 +1,36 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     esGlobalBan - Language File.
+|
+|     $Source: /cvsroot/banned/languages/Russian/lan_manageServerGroups.php,v $
+|     $Revision: 1.0 $
+|     $Date: 2009/10/07 11:56:39 $
+|     $Author: Odonel $
++----------------------------------------------------------------------------+
+*/
+$LANMANAGESERVERGROUPS_001 = 'Действительно удалить ';
+$LANMANAGESERVERGROUPS_002 = '?';
+$LANMANAGESERVERGROUPS_003 = 'Список групп серверов';
+$LANMANAGESERVERGROUPS_004 = 'Имя группы';
+$LANMANAGESERVERGROUPS_005 = 'Описание';
+$LANMANAGESERVERGROUPS_006 = 'Сервера';
+$LANMANAGESERVERGROUPS_007 = 'Админы';
+$LANMANAGESERVERGROUPS_008 = 'Сохранить';
+$LANMANAGESERVERGROUPS_009 = 'Удалить';
+$LANMANAGESERVERGROUPS_010 = "Сервера";
+$LANMANAGESERVERGROUPS_011 = 'Нет серверов';
+$LANMANAGESERVERGROUPS_012 = 'Редактировать админов для  ';
+$LANMANAGESERVERGROUPS_013 = 'Сохранить настройки для ';
+$LANMANAGESERVERGROUPS_014 = 'Удалить ';
+$LANMANAGESERVERGROUPS_015 = 'Добавить новую группу';
+$LANMANAGESERVERGROUPS_016 = 'Имя группы:';
+$LANMANAGESERVERGROUPS_017 = 'Описаание:';
+$LANMANAGESERVERGROUPS_018 = 'Данное имя уже используется';
+$LANMANAGESERVERGROUPS_019 = 'Добавить группу';
+$LANMANAGESERVERGROUPS_021 = 'Внимание: Если вы добавите или удалите группу не забудьте обновить ';
+$LANMANAGESERVERGROUPS_022 = 'Настройки';
+$LANMANAGESERVERGROUPS_023 = '.';
+
+
+?>
